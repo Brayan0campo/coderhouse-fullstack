2ffab1ee3563f72c2727a4bd145bcd3b97652c86
Brayan <ocamporodriguezbrayan@gmail.com>
2023-10-04 12:38:12 -0500

    configurate index

--- coderhouse-backend/desafios-integradores/desafio-integrador-1/src/index.js
+++ coderhouse-backend/desafios-integradores/desafio-integrador-1/src/index.js
@@ -1,16 +1,12 @@
 import * as path from "path";
 import express from "express";
-import { Server } from "socket.io";
+import mongoose from "mongoose";
 import __dirname from "./ultils.js";
 import { engine } from "express-handlebars";
-import cartRouter from "./router/cart.routes.js";
-import prodRouter from "./router/product.routes.js";
-import ProductManager from "./controllers/ProductManager.js";
 
 // Express
-const app = express();
 const PORT = 8080;
-const product = new ProductManager();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,34 +15,28 @@ const httpServer = app.listen(PORT, () => {
   console.log(`Servidor express corriendo en ${PORT}`);
 });
 
-// Socket.io
-const socketServer = new Server(httpServer);
+// Mongoose
+mongoose.connect("mongodb+srv://pruebaCoder:nho3lhDUHKYOwGnX@cluster0.4odjfny.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>{
+  console.log("Conectado correctamente a la base de datos")
+})
+.catch(error => {
+  console.error("Error al conectarse a la base de datos, error" + error)
+})
 
-socketServer.on("connection", (socket) => {
-  console.log("Nuevo Cliente Conectado");
-  socket.on("message", (data) => {
-    console.log(data);
-  });
-
-  socket.on("newProd", (newProduct) => {
-    product.addProduct(newProduct);
-    socketServer.emit("success", "Producto Agregado Correctamente");
-  });
-});
+// Routers
+app.use("/api/carts", cartsRouter)
+app.use("/api/messages", messagesRouter)
+app.use("/api/products", productsRouter)
 
 // Handlebars
 app.engine("handlebars", engine());
-
 app.set("view engine", "handlebars");
 app.set("views", path.resolve(__dirname + "/views"));
-
 app.use("/", express.static(__dirname + "/public"));
-app.use("/realTimeProducts", prodRouter);
 
-app.get("/", async (req, res) => {
-  let allProducts = await product.getProduct();
-  res.render("home", {
-    title: "Handlebars",
-    products: allProducts,
+app.get("/chat", async (req, res) => {
+  res.render("chat", {
+    title: "Chat Implementado con Mongoose",
   });
 });
