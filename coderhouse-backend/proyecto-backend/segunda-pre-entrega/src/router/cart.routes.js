07376002be7a2b023ef8d46e4dbbecbe178b3276
Brayan <ocamporodriguezbrayan@gmail.com>
2023-10-12 11:43:04 -0500

    create routes products

--- coderhouse-backend/proyecto-backend/segunda-pre-entrega/src/router/cart.routes.js
+++ coderhouse-backend/proyecto-backend/segunda-pre-entrega/src/router/cart.routes.js
@@ -8,18 +8,50 @@ cartRouter.get("/", async (req, res) => {
   res.send(await cartManager.getCart());
 });
 
+cartRouter.get("/population/:cid", async (req, res) => {
+  let cartId = req.params.cid;
+  res.send(await cartManager.getCartWithProducts(cartId));
+});
+
 cartRouter.get("/:id", async (req, res) => {
   res.send(await cartManager.getCartById(req.params.id));
 });
 
 cartRouter.post("/", async (req, res) => {
-  res.send(await cartManager.addCart());
+  const newCart = req.body;
+  res.send(await cartManager.addCart(newCart));
 });
 
 cartRouter.post("/:cid/products/:pid", async (req, res) => {
-  let cartId = req.params.cid;
-  let productId = req.params.pid;
+  const cartId = req.params.cid;
+  const productId = req.params.pid;
   res.send(await cartManager.addProductInCart(cartId, productId));
+});
+
+cartRouter.put("/:cid", async (req, res) => {
+  const cartId = req.params.cid;
+  const updateCart = req.body;
+  res.send(await cartManager.updateCart(cartId, updateCart));
+});
+
+cartRouter.put("/:cid/products/:pid", async (req, res) => {
+  const cartId = req.params.cid;
+  const productId = req.params.pid;
+  const updateProduct = req.body;
+  res.send(
+    await cartManager.updateProductInCart(cartId, productId, updateProduct)
+  );
+});
+
+cartRouter.delete("/:cid", async (req, res) => {
+  const cartId = req.params.cid;
+  res.send(await cartManager.deleteCart(cartId));
+});
+
+cartRouter.delete("/:cid/products/:pid", async (req, res) => {
+  const cartId = req.params.cid;
+  const productId = req.params.pid;
+  res.send(await cartManager.deleteProductInCart(cartId, productId));
 });
 
 export default cartRouter;
--- coderhouse-backend/proyecto-backend/segunda-pre-entrega/src/router/product.routes.js
+++ coderhouse-backend/proyecto-backend/segunda-pre-entrega/src/router/product.routes.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { Router } from "express";
 import ProductManager from "../controllers/ProductManager.js";
 
@@ -5,29 +6,92 @@ const productRouter = Router();
 const productManager = new ProductManager();
 
 productRouter.get("/", async (req, res) => {
-  let totalProd = await productManager.getProduct();
-  res.render("realTimeProducts", { title: "Socket", totalProd });
+  const sort = req.query.sort;
+  const category = req.query.category;
+  const available = req.query.available;
+
+  if (sort === undefined) {
+    sort = "asc";
+  }
+
+  if (category === undefined) {
+    category = "";
+  }
+
+  if (available === undefined) {
+    available = "";
+  }
+
+  res.send(
+    await productManager.getProductsAll(null, null, sort, category, available)
+  );
 });
 
 productRouter.get("/:id", async (req, res) => {
-  let id = req.params.id;
-  res.send(await productManager.getProductById(id));
+  try {
+    const productId = req.params.id;
+    const productDetails = await productManager.getProductById(productId);
+    res.render("viewDetails", { product: productDetails });
+  } catch (error) {
+    console.error("Error al obtener el producto:", error);
+    res.status(500).json({ error: "Error al obtener el producto" });
+  }
+});
+
+productRouter.get("/limit/:limit", async (req, res) => {
+  const limit = parseInt(req.params.limit);
+
+  if (isNaN(limit) || limit <= 0) {
+    limit = 10;
+  }
+
+  res.send(await productManager.getProductsByLimit(limit));
+});
+
+productRouter.get("/page/:page", async (req, res) => {
+  const page = parseInt(req.params.page);
+
+  if (isNaN(page) || page <= 0) {
+    page = 1;
+  }
+
+  const productsByPage = 1;
+  res.send(await productManager.getProductsByPage(page, productsByPage));
+});
+
+productRouter.get("/find/query", async (req, res) => {
+  const query = req.query.q;
+  res.send(await productManager.getProductsByQuery(query));
+});
+
+productRouter.get("/order/sort", async (req, res) => {
+  const order = 0;
+
+  if (req.query.sort) {
+    if (req.query.sort === "desc") {
+      order = -1;
+    } else if (req.query.sort === "asc") {
+      order = 1;
+    }
+  }
+
+  res.send(await productManager.getProductsBySort(order));
 });
 
 productRouter.post("/", async (req, res) => {
-  let newProduct = req.body;
+  const newProduct = req.body;
   res.send(await productManager.addProduct(newProduct));
 });
 
 productRouter.put("/:id", async (req, res) => {
-  let id = req.params.id;
-  let updateProduct = req.body;
-  res.send(await productManager.updateProduct(id, updateProduct));
+  const productId = req.params.id;
+  const updateProduct = req.body;
+  res.send(await productManager.updateProduct(productId, updateProduct));
 });
 
 productRouter.delete("/:id", async (req, res) => {
-  let id = req.params.id;
-  res.send(await productManager.deleteProduct(id));
+  const productId = req.params.id;
+  res.send(await productManager.deleteProduct(productId));
 });
 
 export default productRouter;
