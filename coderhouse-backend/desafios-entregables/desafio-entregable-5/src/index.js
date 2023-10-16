3819a995fd472266ac6bddf94afae09b76ec1035
Brayan <ocamporodriguezbrayan@gmail.com>
2023-10-16 10:55:21 -0500

    configurate index routes

--- coderhouse-backend/desafios-entregables/desafio-entregable-5/src/index.js
+++ coderhouse-backend/desafios-entregables/desafio-entregable-5/src/index.js
@@ -1,38 +1,66 @@
 import * as path from "path";
 import express from "express";
+import mongoose from "mongoose";
 import { Server } from "socket.io";
 import __dirname from "./ultils.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import FileStore from "session-file-store";
 import { engine } from "express-handlebars";
 import cartRouter from "./router/cart.routes.js";
+import userRouter from "./router/user.routes.js";
 import prodRouter from "./router/product.routes.js";
+import CartManager from "./controllers/CartManager.js";
 import ProductManager from "./controllers/ProductManager.js";
 
 // Express
-const app = express();
 const PORT = 8080;
-const product = new ProductManager();
+const app = express();
+const fileStorage = FileStore(session);
+const productManager = new ProductManager();
+const cartManager = new CartManager();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const httpServer = app.listen(PORT, () => {
-  console.log(`Servidor express corriendo en ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Express server running on port ${PORT}`);
 });
 
-// Socket.io
-const socketServer = new Server(httpServer);
-
-socketServer.on("connection", (socket) => {
-  console.log("Nuevo Cliente Conectado");
-  socket.on("message", (data) => {
-    console.log(data);
+// Mongoose
+mongoose
+  .connect(
+    "mongodb+srv://pruebaCoder:nho3lhDUHKYOwGnX@cluster0.4odjfny.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database, error" + error);
   });
 
-  socket.on("newProd", (newProduct) => {
-    product.addProduct(newProduct);
-    socketServer.emit("success", "Producto Agregado Correctamente");
-  });
-});
+// Session Mongo
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl:
+        "mongodb+srv://pruebaCoder:nho3lhDUHKYOwGnX@cluster0.4odjfny.mongodb.net/?retryWrites=true&w=majority",
+      mongoOptions: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+      ttl: 3600,
+    }),
+    secret: "SecretKey",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Routes
+app.use("/api/carts", cartRouter);
+app.use("/api/products", prodRouter);
+app.use("/api/sessions", userRouter);
 
 // Handlebars
 app.engine("handlebars", engine());
@@ -41,12 +69,51 @@ app.set("view engine", "handlebars");
 app.set("views", path.resolve(__dirname + "/views"));
 
 app.use("/", express.static(__dirname + "/public"));
-app.use("/realTimeProducts", prodRouter);
 
-app.get("/", async (req, res) => {
-  let allProducts = await product.getProduct();
-  res.render("home", {
-    title: "Handlebars",
-    products: allProducts,
+app.get("/products", async (req, res) => {
+  if (!req.session.emailUser) {
+    return res.redirect("/login");
+  }
+  let products = await productManager.getProducts();
+  products = products.map((product) => product.toJSON());
+  res.render("viewProducts", {
+    title: "Products",
+    products: products,
+    email: req.session.emailUser,
+    user: req.session.roleUser,
+  });
+});
+
+app.get("/carts/:cid", async (req, res) => {
+  let id = req.params.cid;
+  let carts = await cartManager.getCartWithProducts(id);
+  res.render("viewCart", {
+    title: "Carts",
+    carts: carts,
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login", {
+    title: "Login",
+  });
+});
+
+app.get("/register", (req, res) => {
+  res.render("register", {
+    title: "Register",
+  });
+});
+
+app.get("/profile", (req, res) => {
+  if (!req.session.emailUser) {
+    return res.redirect("/login");
+  }
+  res.render("profile", {
+    title: "Profile",
+    firstName: req.session.firstName,
+    lastName: req.session.lastName, 
+    email: req.session.emailUser,
+    user: req.session.roleUser,
   });
 });
