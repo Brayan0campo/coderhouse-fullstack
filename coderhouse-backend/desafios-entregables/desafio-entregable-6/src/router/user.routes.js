7ef3c4abc35f2ea0f2fa054b2b39237a58795838
Brayan <ocamporodriguezbrayan@gmail.com>
2023-10-26 18:03:05 -0500

    update user routes

--- coderhouse-backend/desafios-entregables/desafio-entregable-6/src/router/user.routes.js
+++ coderhouse-backend/desafios-entregables/desafio-entregable-6/src/router/user.routes.js
@@ -1,8 +1,80 @@
+import express from "express";
+import passport from "passport";
 import { Router } from "express";
 import UserManager from "../controllers/UserManager.js";
+import { hashPassword, validatePassword } from "../utils/";
 
 const userRouter = Router();
 const userManager = new UserManager();
+
+userRouter.post(
+  "/register",
+  passport.authenticate("register", { failureRedirect: "/fail-register" }),
+  async (req, res) => {
+    try {
+      const { firstName, lastName, email, age, password, role } = req.body;
+      if (!firstName || !lastName || !email || !age || !password || !role) {
+        return res.status(400).send("Missing Required Fields");
+      }
+      res.redirect("/login");
+    } catch (error) {
+      res.status(500).send("Error Registering User: " + error.message);
+    }
+  }
+);
+
+userRouter.get("/fail-register", async (req, res) => {
+  res.send(error, "Error Registering User");
+});
+
+userRouter.post(
+  "/login",
+  passport.authenticate("login", { failureRedirect: "/fail-login" }),
+  async (req, res) => {
+    try {
+      if (!req.user) {
+        return res
+          .status(400)
+          .send({ status: "Login Error", error: "Invalid Credentials" });
+      }
+
+      if (req.user.role === "admin") {
+        req.session.email = req.user.email;
+        req.session.firstName = req.user.firstName;
+        req.session.lastName = req.user.lastName;
+        req.session.role = req.user.role;
+        res.redirect("/profile");
+      } else {
+        req.session.email = req.user.email;
+        req.session.role = req.user.role;
+        res.redirect("/products");
+      }
+    } catch (error) {
+      res.status(500).send("Error logging in: " + error.message);
+    }
+  }
+);
+
+userRouter.get("/fail-login", async (req, res) => {
+  res.send(error, "Error logging in");
+});
+
+userRouter.get(
+  "/github",
+  passport.authenticate("github", { scope: ["user:email"] }),
+  async (req, res) => {}
+);
+
+userRouter.get(
+  "/githubcallback",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  async (req, res) => {
+    req.session.user = req.user;
+    req.session.email = req.session.user.email;
+    req.session.role = req.session.user.role;
+    res.redirect("/products");
+  }
+);
 
 userRouter.get("/logout", async (req, res) => {
   req.session.destroy((error) => {
@@ -11,40 +83,6 @@ userRouter.get("/logout", async (req, res) => {
     }
     res.redirect("../../login");
   });
-});
-
-userRouter.post("/register", (req, res) => {
-  try {
-    let newUser = req.body;
-    userManager.createUser(newUser);
-    res.redirect("/login");
-  } catch (error) {
-    res.status(500).send("Error registering user: " + error.message);
-  }
-});
-
-userRouter.post("/login", async (req, res) => {
-  try {
-    let email = req.body.email;
-    const data = await userManager.validateUser(email);
-    if (data.password === req.body.password) {
-      if (data.role === "admin") {
-        req.session.emailUser = email;
-        req.session.firstName = data.firstName;
-        req.session.lastName = data.lastName;
-        req.session.roleUser = data.role;
-        res.redirect("/profile");
-      } else {
-        req.session.emailUser = email;
-        req.session.roleUser = data.role;
-        res.redirect("/products");
-      }
-    } else {
-      res.redirect("../../login");
-    }
-  } catch (error) {
-    res.status(500).send("Error logging in: " + error.message);
-  }
 });
 
 export default userRouter;
