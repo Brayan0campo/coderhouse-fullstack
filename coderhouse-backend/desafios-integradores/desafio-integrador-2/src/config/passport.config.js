9161b35bd3477832fce853d951d4fe327f58c1d8
Brayan <ocamporodriguezbrayan@gmail.com>
2023-11-04 23:28:46 -0500

    configurate passport

--- coderhouse-backend/desafios-integradores/desafio-integrador-2/src/config/passport.config.js
+++ coderhouse-backend/desafios-integradores/desafio-integrador-2/src/config/passport.config.js
@@ -1,2 +1,36 @@
+import jwt from "passport-jwt";
 import passport from "passport";
 import local from "passport-local";
+
+const LocalStrategy = local.Strategy;
+const JwtStrategy = jwt.Strategy;
+const ExtractJwt = jwt.ExtractJwt;
+
+const cookieExtractor = (req) => {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies["cookie"];
+  }
+  return token;
+};
+
+const initializePassport = () => {
+  passport.use(
+    "jwt",
+    new JwtStrategy(
+      {
+        jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]),
+        secretOrKey: "SecretKey",
+      },
+      async (jwtPayload, done) => {
+        try {
+          return done(null, jwtPayload);
+        } catch (error) {
+          return done(error);
+        }
+      }
+    )
+  );
+};
+
+export default initializePassport;
